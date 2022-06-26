a2a32ada23d0cc754d7a322a0351168d2e8d2852
João Vitor Lopes Fagundes <70524677+joaovitorfod4@users.noreply.github.com>
2022-06-26 20:57:43 -0300

    Add files via upload

---
+++ javascripting/function-arguments.js
@@ -0,0 +1,5 @@
+function math(cavalo, uiiii, calma){
+    return uiiii * calma + cavalo;
+}
+
+console.log(math(53, 61, 67));
---
+++ javascripting/revising-strings.js
@@ -0,0 +1,5 @@
+let pizza = 'pizza is alright';
+
+let pizza2 = pizza.replace('alright', 'wonderful');
+
+console.log(pizza2);
